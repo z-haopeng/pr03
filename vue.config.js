640de25f85243d09993ee88b4ec386520aa28933
z-haopeng <kevinzhou2222@gmail.com>
2022-12-03 18:46:55 -0500

    Fix production build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/pr03/" : "/",
+  outputDir: 'docs',
 
   pluginOptions: {
     vuetify: {
